edd6199b6ffe21939df3bdd975d8fa9c9fd9c645
isaactaclia <icolmenares@taclia.com>
2023-06-19 11:57:45 +0200

    Fin sección 7

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
@@ -17,6 +19,8 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     CommonModule,
+    RouterModule,
+    FormsModule
   ]
 })
 export class AuthModule { }
